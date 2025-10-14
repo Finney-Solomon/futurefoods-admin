025e35167f9406cf81f0dad9f675db4e6f62c86c
“Finney <finney.solomon.1@gmail.com>
2025-10-14 19:19:30 +0530

    Deleted and API's Folder

--- Backend/src/schemas/categorySchemas.js
+++
@@ -1,9 +0,0 @@
-// src/schemas/categorySchemas.js
-import { z } from "zod";
-export const categoryCreateSchema = z.object({
- name: z.string().min(2),
- slug: z.string().min(2),
- description: z.string().optional(),
- isActive: z.boolean().optional(),
-});
-export const categoryUpdateSchema = categoryCreateSchema.partial();
